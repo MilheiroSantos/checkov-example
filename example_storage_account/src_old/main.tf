213c329875d99425dde4210b80bb5e3b16a937a7
Pedro.Santos <pedro.santos@unicoeding.com>
2021-01-22 14:57:22 +0100

    switch old src with new

--- example_storage_account/src_old/main.tf
+++ example_storage_account/src_old/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "=2.43.0"
+  version = "=2.20.0"
   features {}
 }
 
@@ -13,19 +13,9 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_storage_account" "this" {
-  #checkov:skip=CKV_AZURE_33
-
   name                     = "mystorageaccount"
   account_replication_type = "LRS"
   account_tier             = "Premium"
   location                 = azurerm_resource_group.this.location
   resource_group_name      = azurerm_resource_group.this.name
-
-  min_tls_version           = "TLS1_2"
-  enable_https_traffic_only = true
-
-  network_rules {
-    default_action = "Deny"
-    ip_rules       = ["80.0.2.0"]
-  }
 }
